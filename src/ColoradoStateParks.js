1ae0f7109182fca9f29672cb01b06a6a29ad7b5e
Serje <Serje.havandjian@gmail.com>
2022-05-15 21:00:42 -0400

    commit tree e7d83d10659315afe32748cb690225de5a3af7f7

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde"
+import * as AllVariables from "./parks/RockyMountain"
+
+console.log(AllVariables.trees)
+AllVariables.wildlife()
+AllVariables.elevation()
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return <h1>
+    <MesaVerde />
+  </h1>;
 }
+
+
+
+
+export default ColoradoStateParks
--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,11 +1,13 @@
-const trees = "Aspen and Pine";
 
-function wildlife() {
+
+
+export const trees = "Aspen and Pine";
+
+export function wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
 }
 
-function elevation() {
+export function elevation() {
   console.log("9583 ft");
 }
 
-export { trees, wildlife };
